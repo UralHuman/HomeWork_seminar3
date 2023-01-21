38aa7eca71b6889ab6142ff5c8da6ee4fcba23c1
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-01-21 14:03:24 +0500

    creat_prog_1

---
+++ HomeWork_1_seminar_3/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.Write("Введите пятизначое счисло--> ");
+
+int a = Convert.ToInt32(System.Console.ReadLine());
+
+int num1 = (a/10000);
+int num2 = (((a/100)/10)%10);
+int num4 = ((a/10)%10);
+int num5 = (a%10);
+
+if (num1==num5&&num2==num4)
+{
+    System.Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    System.Console.WriteLine("Чисо не яыляется палиндромом");
+}
+
+
+
