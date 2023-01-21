1fc7ff67264447e19fb9c85116c9607e506025ca
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-01-22 00:03:37 +0500

    create code_2.1

--- HomeWork_2_seminar_3/Program.cs
+++ HomeWork_2_seminar_3/Program.cs
@@ -1,17 +1,14 @@
 ﻿Console.Clear();
-System.Console.Write("Введите кординаты x1--> ");
+System.Console.WriteLine("Введите кординаты: x1,x2,y1,y2,z1,z2--> ");
 double x1 = Convert.ToDouble(Console.ReadLine());
-System.Console.Write("Введите кординаты x2--> ");
 double x2 = Convert.ToDouble(Console.ReadLine());
-System.Console.Write("Введите кординаты y1--> ");
 double y1 = Convert.ToDouble(Console.ReadLine());
-System.Console.Write("Введите кординаты y2--> ");
 double y2 = Convert.ToDouble(Console.ReadLine());
-System.Console.Write("Введите кординаты z1--> ");
 double z1 = Convert.ToDouble(Console.ReadLine());
-System.Console.Write("Введите кординаты z2--> ");
 double z2 = Convert.ToDouble(Console.ReadLine());
-double AB = (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2);
 
+double AB = Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2));
 
-System.Console.WriteLine($"{(Math.Sqrt(AB))}" );
+double truncated = Math.Truncate(AB * 100) / 100;
+
+System.Console.WriteLine($"Расстояние между точкой А и точкой В--> {(truncated)}" );
