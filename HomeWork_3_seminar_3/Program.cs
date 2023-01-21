a032d4ecfd2f0d47c6b18c6a03a5f93306e23f2d
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-01-22 00:19:50 +0500

    create code3

---
+++ HomeWork_3_seminar_3/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.Write("Введите число--> ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i=1;i<=n;i++)
+{
+    System.Console.Write($" {i*i*i} ");
+}
+System.Console.WriteLine();
